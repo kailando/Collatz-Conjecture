5718d88c6a0378f22230e21e8e4e8cb0992f25f9
kailando <94944679+kailando@users.noreply.github.com>
2024-01-03 20:45:04 -0600

    Rollback main.py

--- main.py
+++ main.py
@@ -1,26 +1,51 @@
-from sys import argv
-codeChars, mem_size, mem, pointer, ram, ptr, stack=[">", "<", "-", "+", "^", ".", ",", "[", "]", "#", "!"], 8, [0] * 8, 0, 0, 0, []
-try: code=''.join(char for char in open(argv[1]).read() if char in codeChars)
-except IndexError: code=''.join(char for char in input("Brainf**k V.2 code:\n") if char in codeChars)
-while ptr<len(code):
-    char, ptr=code[ptr], ptr+1
-    if char==codeChars[0]: pointer+=1
-    elif char==codeChars[1]: pointer-=1
-    elif char==codeChars[2]: ram-=1
-    elif char==codeChars[3]: ram+=1
-    elif char==codeChars[4]: mem[pointer]=ram
-    elif char==codeChars[5]: ram=mem[pointer]
-    elif char==codeChars[6]: print(chr(mem[pointer]), end="")
-    elif char==codeChars[7]:
-        if mem[pointer]==0:
-            stack.append(ptr)
-            while stack:
-                ptr+=(code[ptr]=="[")-(code[ptr]=="]")
-                stack.append(ptr) if code[ptr]=="[" else stack.pop()
-    elif char==codeChars[8]:
-            ((ptr:=stack[-1]) if mem[pointer]!=0 else (stack.pop()))
-        else:
-            stack.pop()
-    elif char==codeChars[9]: ram=min(255, max(0, int(input("0-255: "))))
-    elif char==codeChars[10]: exit()
-    mem_size, pointer, ram, mem, ram=mem_size+pointer>=mem_size, max(pointer, 0), max(0, min(255, ram)), [min(255, m) for m in mem], min(255, max(0, ram))
+import sys # Add 2 extra arguments (starting seed and ending seed) for brute-forcing.
+# 3x+1 problem
+# The 3x+1 rule is: If x is odd, x=3*x+1. If x is even, x=x/2.
+global isEven
+isEven=lambda number : not number%2
+
+scriptUsage="3x+1" # Default: "3x+1", change for development use.
+# Tests isEven lambda.
+# For development use only.
+print(not isEven(1)) and (isEven(2)) and (not isEven(3)) and (isEven(4)) and (not isEven(5)) if scriptUsage=="isEven" else None
+
+
+# Calculates next number based off of the 3x+1 rule.
+def nextInt(current):
+    return int(current/2 if isEven(current) else current*3+1)
+
+# The thing that calculates the 3x+1 problem.
+def problem(seed):
+    maximum=0
+    steps=0
+    current=seed
+    ints=[current]
+    breakingInt=1
+    while current != breakingInt:
+        ints.append(current:=nextInt(current))
+        if current>maximum:
+            maximum=int(current)
+        steps+=1
+        continue
+    return maximum, ints, steps
+
+if len(sys.argv)!=3:
+    out=problem(int(input("Seed: ")))
+    print(f"Max: {out[0]}")
+    print(f"Integers: {out[1]}")
+    print(f"Steps: {out[2]}")
+else:
+    import pyperclip
+    answers=[]
+    for seed in range(int(sys.argv[1]), int(sys.argv[2])):
+        answer=problem(seed)
+        answers.append(f"Seed: {seed}")
+        answers.append(f"Max: {answer[0]}")
+        answers.append(f"Ints: {answer[1]}")
+        answers.append(f"Steps: {answer[2]}")
+        continue
+    answers=str(answers)
+    contents=pyperclip.paste()
+    pyperclip.copy(answers)
+    print(f"Answers copied to clipboard. Clipboard content: {content}")
+    print("Brute-forcing done.")
